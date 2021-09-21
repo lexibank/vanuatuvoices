d4dbfdf2f53027465a460b44d899fcb87cef967e
Hans-Jörg Bibiko <hans_joerg_bibiko@eva.mpg.de>
2021-09-21 16:02:31 +0200

    cdstar url move to eva

--- lexibank_vanuatuvoices.py
+++ lexibank_vanuatuvoices.py
@@ -57,7 +57,7 @@ class Dataset(BaseDataset):
             {
                 'name': 'ID',
                 'propertyUrl': 'http://cldf.clld.org/v1.0/terms.rdf#id',
-                'valueUrl': 'https://cdstar.shh.mpg.de/bitstreams/{objid}/{fname}',
+                'valueUrl': 'https://cdstar.eva.mpg.de/bitstreams/{objid}/{fname}',
             },
             'phonetic_transcriptions',
             'recording',
